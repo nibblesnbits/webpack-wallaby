9313a2b396906f4fa5bae1f5dcc036a0ae0ffb6c
Stephen Collins <stephen.collins@centeva.com>
2017-02-23 00:21:08 -0500

    added instrument:false example

--- wallaby.js
+++ wallaby.js
@@ -1,47 +1,48 @@
-const wallabyWebpack = require('wallaby-webpack');
-
 module.exports = function (wallaby) {
-
-  const webpackPostprocessor = wallabyWebpack({
-    // webpack options
-
-    externals: {
-      // Use external version of React instead of rebuilding it
-      "react": "React"
-    },
-    resolve: {
-      extensions: ['.js']
-    }
-  });
 
   return {
     files: [
-      { pattern: 'node_modules/react/dist/react-with-addons.js', instrument: false },
-      { pattern: 'src/**/*.js', load: false, instrument: true },
+      { pattern: 'src/**/*.js' },
       { pattern: 'src/**/*.spec.js', ignore: true },
+      { pattern: 'src/store/*.js', ignore: true },
+      { pattern: 'src/index*.js', ignore: true },
+      { pattern: 'src/routes*.js', ignore: true },
+      { pattern: 'src/webpack*.js', ignore: true },
+      { pattern: 'src/*.prod.js', ignore: true },
+      { pattern: 'src/constants/*.js', instrument: false },
+      { pattern: 'src/sagas/rootSaga.js', ignore: true },
+      { pattern: 'src/config*', instrument: false },
     ],
 
     tests: [
-      { pattern: 'src/**/*.spec.js', load: false },
+      { pattern: 'src/**/*.spec.js' },
     ],
+
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
 
     compilers: {
       '**/*.js': wallaby.compilers.babel({
         "presets": [
           "latest",
           "react",
-          "stage-2"
+          "stage-2",
         ],
       }),
     },
 
-    postprocessor: webpackPostprocessor,
-
     debug: true,
 
     setup: function () {
-      console.log(window.$);
-      window.__moduleBundler.loadTests();
-    }
+      process.env.NODE_ENV = 'test';
+      require('jsdom-global/register');
+      require('babel-polyfill');
+
+      ['.css', '.scss', '.png', '.jpg'].forEach(ext => {
+        require.extensions[ext] = () => null;
+      });
+    },
   };
 };
